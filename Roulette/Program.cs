34c91b5d2afd97fa282af1ed5aa900fbfec6884f
ortiz92105@gmail.com <ortiz92105@gmail.com>
2020-03-31 14:08:40 -0700

    Cleaned code and added a new method

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -3,7 +3,6 @@ using System.Linq;
 
 namespace Roulette
 {
-
     class Program
     {
         public static int NumRandomizer()    //method to generate a random number
@@ -12,12 +11,18 @@ namespace Roulette
             int randomIndex = random.Next(0, 38); //randomizes numbers from 0 to 37
             return randomIndex;
         }
-
-            //int[] RedNums = new int[18] { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
-            //int[] BlackNums = new int[18] { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
-            static void Main(string[] args)
+        public static char CharRandomizer()
         {
-
+            char[] chars = "abcdefghijkl".ToCharArray();
+            Random r = new Random();
+            int randomIndex = r.Next(chars.Length);
+            return chars[randomIndex];
+        }
+        //int[] RedNums = new int[18] { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+        //int[] BlackNums = new int[18] { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+        static void Main(string[] args)
+        {
+           // Console.ForegroundColor = ConsoleColor.Yellow;
             // int[] rouletteNum = new int[38];  //number array
             //
             // string[] rouletteColor = new string[] { "black", "red" }; //colors
@@ -39,7 +44,6 @@ namespace Roulette
             Console.WriteLine("\t\t(8) 6 Numbers: double rows, e.g., 1/2/3/4/5/6 or 22/23/24/25/26/26");
             Console.WriteLine("\t\t(9) Split: at the edge of any two contiguous numbers, e.g., 1/2, 11/14, and 35/36");
             Console.WriteLine("\t\t(10) Corner: at the intersection of any four contiguous numbers, e.g., 1/2/4/5, or 23/24/26/27");
-
             // Random rnd = new Random();
             //int caseSwitch = rnd.Next(1, 4);
 
@@ -48,68 +52,73 @@ namespace Roulette
             //int[] RedNums = new int[18] { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
             //int[] BlackNums = new int[18] { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
             //}
-
-            int UserChoice;
-            Console.WriteLine("\t\t ________________");
-            Console.WriteLine("\t\t|                |" );
-            Console.WriteLine("\t\t| Select 1 - 10: |");
-            Console.WriteLine("\t\t|________________|");
-
-            UserChoice = int.Parse(Console.ReadLine());
-
-            int caseSwitch = UserChoice;
-            switch (caseSwitch)
+            try
             {
-                case 1:
-                    Numbers();
-                   
+                int UserChoice;
+                Console.WriteLine("\t\t\nSelect 1 - 10: ");
+                UserChoice = int.Parse(Console.ReadLine());
 
-                    //TODO add "you win/lose" if numbers match or not
-                    break;
-                case 2:
-                    EvenOddBet();
-                    break;
-                case 3:
-                    RedsBlacks();
+                // int caseSwitch = 5;
+                int caseSwitch = UserChoice;
+                switch (caseSwitch)
+                {
+                    case 1:
+                        Numbers();
+                        break;
+                    case 2:
+                        EvenOddBet();
+                        break;
+                    case 3:
+                        RedsBlacks();
 
-                    break;
-                case 4:
-                    LowsHighs();
-                    break;
-                case 5:
-                    Dozens();
-                    break;
-                case 6:
-                    Console.WriteLine("6");
-                    break;
-                case 7:
-                    Console.WriteLine("7");
-                    break;
-                case 8:
-                    Console.WriteLine("8");
-                    break;
-                case 9:
-                    Console.WriteLine("9");
-                    break;
-                case 10:
-                    Console.WriteLine("10");
-                    break;
+                        break;
+                    case 4:
+                        LowsHighs();
+                        break;
+                    case 5:
+                        Dozens();
+                        break;
+                    case 6:
+                        Columns();
+                        break;
+                    case 7:
+                        Street();
+                        break;
+                    case 8:
+                        Console.WriteLine("8");
+                        break;
+                    case 9:
+                        Console.WriteLine("9");
+                        break;
+                    case 10:
+                        Console.WriteLine("10");
+                        break;
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("This is Not a valid option");
             }
         }
-        public static int Numbers()    //method to generate a random number
-        {int UserNum =0;
+        public static int Numbers() 
+        {
+            bool CompareNum = true;
+            int UserNum = 0;
             Console.WriteLine($"You placed a bet on (1)");
             Console.WriteLine("Select a number: ");
             UserNum = Convert.ToInt32(Console.ReadLine());
-
-            // Console.WriteLine($"You chose: {NumRandomizer()}"); //can make user select number
-            Console.WriteLine($"the winning number is: {NumRandomizer()}");
-             
-            Console.WriteLine("you lose");
-            Random random = new Random();
-            int randomIndex = random.Next(0, 38); //randomizes numbers from 0 to 37
-            return randomIndex;
-            //todo asign 37 to 00
+            int HouseNum = NumRandomizer();
+            Console.WriteLine($"the winning number is: {HouseNum}");
+            if (UserNum != HouseNum)
+            {
+                Console.WriteLine("you lose");
+                CompareNum = false;
+            }
+            else {
+                Console.WriteLine("you win");
+                CompareNum = true;
+            }
+            return UserNum;                  
         }
         public static void EvenOddBet()     //method to place a bet and then house chooses number
         {
@@ -120,7 +129,6 @@ namespace Roulette
 
             //house chooses number with NumRandomizer method
             int house = NumRandomizer();
-           
 
             Console.WriteLine(house);
             //Console.WriteLine($"The house number is: {NumRandomizer()}");    //i am not using this
@@ -149,12 +157,8 @@ namespace Roulette
             //int[] BlackNums = new int[18] { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
 
             int house = NumRandomizer();
-            //int house = 4;
-            Console.WriteLine(house);
-            //return (house);
-            // for (int i = 0; i > 37; i++)     
+            Console.WriteLine(house);   
             foreach (int Num in RedNums)
-
                 if (house == Num)
                 {
                     Console.WriteLine("REDS WINS");
@@ -176,7 +180,7 @@ namespace Roulette
 
             int result1 = LowNums.Length;
             foreach (int compareNum in LowNums)
-                if (house != compareNum && myInt!=compareNum)
+                if (house != compareNum && myInt != compareNum)
                 {
                     Console.WriteLine($"house number is: {compareNum}");
                     Console.WriteLine("Lows WIN");
@@ -188,27 +192,113 @@ namespace Roulette
         public static bool Dozens()
         {
             int UserNum = 0;
-            int[] Lows = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-            int[] Mids = { 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
-            int[] Highs = { 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
+
             Console.WriteLine("Select (1): for Row 1-12...(13): for Row 13-24...(25) for Row 25-36");
             UserNum = Convert.ToInt32(Console.ReadLine());
+            int House = NumRandomizer();
+            Console.WriteLine($"winning number is:{House}");
 
-            int House = 5;
-            foreach (int check1 in Lows)
-          
-                 //if(House!=check1 && UserNum!=check1)
-                if (House >= 1 && House <= 12 && UserNum >= 1 && UserNum <= 12)
-                    
-                {
+            if (House >= 1 && House <= 12 && UserNum >= 1 && UserNum <= 12)    
+            {
+                Console.WriteLine("win");
+                return true;
+            }   
+            else if (House >= 13 && House <= 24 && UserNum >= 13 && UserNum <= 24)
+                {     
                     Console.WriteLine("win");
                     return true;
                 }
-                Console.WriteLine("lose");
-                return false;                
+            else if (House >= 25 && House<=36 && UserNum >=25 && UserNum<=36)
+                {       
+                    Console.WriteLine("win");
+                    return true;
+                }
+            Console.WriteLine("lose");
+            return false;
             }
+        public static void Columns()
+        {
+            int house = NumRandomizer();
+            string guess;
+            Console.WriteLine("(g) for 1st 12,  (h) for 2nd 12, (i) for 3rd 12");           
+            guess = Convert.ToString(Console.ReadLine());
+            int[] firstColumn = { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34 };
+                int[] secondColumn = { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35 };
+                int[] thirdColumn = { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36 };          
+            switch (guess)
+            {
+                case "g":
+                    foreach (int ColumnNum in firstColumn)
+                        if (house == ColumnNum)                            
+                        {
+                            Console.WriteLine($"winning number is: {house}");
+                            Console.WriteLine("you win");
+                            return; }
+                        else { Console.WriteLine("you lose"); 
+                            return; }
+                    break;
+                case "h":
+                    foreach (int ColumnNum in secondColumn)
+                        if (house == ColumnNum)
+                        { Console.WriteLine($"winning number is: {house}");
+                            Console.WriteLine("you win"); 
+                            return; }
+                        else { 
+                            Console.WriteLine("you lose"); 
+                            return; }
+                    break;
+                case "i":
+                    foreach (int ColumnNum in thirdColumn)
+                        if (house == ColumnNum)
+                        {
+                            Console.WriteLine($"winning number is: {house}");
+                            Console.WriteLine("you win");
+                            return; }
+                        else { Console.WriteLine("you lose");
+                            return;
+                        }
+                    break;
+            }
+            return;
+        }
+        public static bool Street()
+        {
+          int house = 3;
+            Console.WriteLine( $"computer chose: {house}" );
+          char guess;
+           // int[] streeNums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
+            Console.WriteLine("Bet on a street, Select (a)for 1-3 (b) 4-6 (c) 7-9 (d) 10-12 (e) 13-15 (f) 16-18 (g)19-21" +
+                "(h) 22-24 (i) 25-27 (j)28-30 (k)31-33 (l)34-36 ");
+            guess = Convert.ToChar(Console.ReadLine());
+
+          //  int[] guess;
+           // if ( guess >0 && guess <4)             
+                int [] a = { 1, 2, 3 };
+                int[] b = { 4, 5, 6 };
+                int[] c = { 7, 8, 9 };
+                int[] d = { 10, 11, 12 };
+                int[] e = { 13, 14, 15 };
+                int[] f = { 16, 17, 18 };
+                int[] g = { 19, 20, 21 };
+                int[] h = { 22, 23, 24 };
+                int[] i = { 25, 26, 27 };
+                int[] j = { 28, 29, 30 };
+                int[] k = { 31, 32, 33 };
+                int[] l = { 34, 35, 36 };
+            if(house == guess)
+            {
+                Console.WriteLine("you win");
+                return true;              
+            }
+            else
+            {
+                Console.WriteLine("you lose");
+                return false;
+            }
+
         }
     }
+}
 
 
 
